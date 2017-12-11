88d5d5514a4666f293c091663010a08581dfbf30
deepaliprasad <deepaliprasad123@gmail.com>
2017-12-11 15:00:06 +0530

    using layout component in app for wrapping everying inside layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Layout from './components/Layouts/Layout'
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Layout>
+            <p> Test </p>
+        </Layout>
       </div>
     );
   }
