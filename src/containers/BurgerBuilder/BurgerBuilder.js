a947ab1ebceecc4cfb963a4cfc186d0f18081d2a
deepaliprasad <deepaliprasad123@gmail.com>
2017-12-14 15:38:06 +0530

    creating a state burger object and sending the state to the burger component throug props

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import Aux from '../../hoc/Aux';
+import Aux from  '../../hoc/Aux';
 import Burger from '../../components/Burgers/Burgers';
 
 class BurgerBuilder extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            ingredients: {
+                'salad': 0,
+                'cheese': 0,
+                'bacon': 0,
+                'meat': 0
+            }
+        }
+    }
     render() 
     {
         return(
             <Aux>
-                <Burger />
+                <Burger ingredient={this.state.ingredients} >
+                </Burger>
                 <div> Build Controls </div>
             </Aux>
         );
