7defa49e0e5bf9ee8e29fae7e7a88254c0e07dc6
deepaliprasad <deepaliprasad123@gmail.com>
2017-12-15 15:43:25 +0530

    added a onClick button handler for the order now button for the order summary preview

--- src/components/Burgers/BuildControls/BuildControls.js
+++ src/components/Burgers/BuildControls/BuildControls.js
@@ -16,7 +16,7 @@ const buildControls = (props) => (
                                         removed={()=> props.removeIngredient(ctrl.type)}
                                         disabled={props.disabled[ctrl.type]}/>
         ))}
-    <button className={classes.OrderButton} disabled={!props.purchasable}> ORDER NOW </button>
+    <button className={classes.OrderButton} disabled={!props.purchasable} onClick={props.ordered}> ORDER NOW </button>
     </div>
 );
 
