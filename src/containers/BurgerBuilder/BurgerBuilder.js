c9b85c5ad897f6c6049a0f37a2a15300a0356cf3
deepaliprasad <deepaliprasad123@gmail.com>
2017-12-15 11:58:07 +0530

    function add handler for adding the ingredient also total price tracking has been implemented

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Aux from  '../../hoc/Aux';
 import Burger from '../../components/Burgers/Burgers';
 import BuildControls from '../../components/Burgers/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
+
 class BurgerBuilder extends Component {
     constructor (props) {
         super(props);
@@ -12,16 +19,41 @@ class BurgerBuilder extends Component {
                 'cheese': 0,
                 'bacon': 0,
                 'meat': 0
-            }
+            },
+
+            totalPrice: 4
+            
         }
     }
+
+    addIngredientHandler = (type) => {
+        const oldTypeCount = this.state.ingredients[type];
+        const updatedCount = oldTypeCount + 1;
+        const updatedIngredient = {
+            ...this.state.ingredients
+        };
+
+        updatedIngredient[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice: newPrice,
+                        ingredients: updatedIngredient });
+        
+    }
+        
+
+    removeIngredientHandler =(type) => {
+
+    } 
+    
     render() 
     {
         return(
             <Aux>
                 <Burger ingredient={this.state.ingredients} >
                 </Burger>
-                <BuildControls />
+                <BuildControls addIngredient={this.addIngredientHandler}/>
             </Aux>
         );
     }
