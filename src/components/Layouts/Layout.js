686b978402dcf1cab4bbc52cfe99240081be2e77
deepaliprasad <deepaliprasad123@gmail.com>
2017-12-18 23:14:52 +0530

    incorporated toolbar changes to the layout

--- src/components/Layouts/Layout.js
+++ src/components/Layouts/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
 import styles from './Layout.css';
-
+import TooBar from '../Navigation/ToolBar/ToolBar';
 
 const layout = (props) => (
     <Aux>
-        <div> ToolBar, SideBar, BackDrop </div>
+        <TooBar />
             <main className={styles.Content}> 
                 {props.children}
             </main>
