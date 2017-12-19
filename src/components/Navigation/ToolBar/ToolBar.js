6a46ae2b18968a5ff3950b4c751ecd330e556c02
deepaliprasad <deepaliprasad123@gmail.com>
2017-12-19 15:48:33 +0530

    setting logo class from toolbar.css

--- src/components/Navigation/ToolBar/ToolBar.js
+++ src/components/Navigation/ToolBar/ToolBar.js
@@ -6,7 +6,9 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 const toolBar = (props) => (
     <header className={classes.TooBar}>
         <div> MENU </div>
-        <Logo />
+        <div className={classes.Logo}> 
+            <Logo />
+        </div>
         <nav>
             <NavigationItems />
         </nav>
