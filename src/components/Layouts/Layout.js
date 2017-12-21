4fa5977459c22e8bfde73ac54021d66eeec24073
deepaliprasad <deepaliprasad123@gmail.com>
2017-12-21 13:21:40 +0530

    changing the component from stateless function to statefull class

--- src/components/Layouts/Layout.js
+++ src/components/Layouts/Layout.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import styles from './Layout.css';
 import TooBar from '../Navigation/ToolBar/ToolBar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
-const layout = (props) => (
-    <Aux>
-        <TooBar />
-        <SideDrawer />
-            <main className={styles.Content}> 
-                {props.children}
-            </main>
-    </Aux>
-);
+class Layout extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showSideDrawer: true
+        }
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+    render(){
+        return (
+            <Aux>
+            <TooBar />
+            <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+                <main className={styles.Content}> 
+                    {this.props.children}
+                </main>
+        </Aux>
+        );
+    }
+};
 
-export default layout;
+export default Layout;
