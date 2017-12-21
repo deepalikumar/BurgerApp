f7fed96a2968525a88dc1fc43fb0f244e3efda82
deepaliprasad <deepaliprasad123@gmail.com>
2017-12-21 14:56:39 +0530

    added toggle function and pasing that function as a props to the toolbar

--- src/components/Layouts/Layout.js
+++ src/components/Layouts/Layout.js
@@ -10,13 +10,19 @@ class Layout extends Component {
             showSideDrawer: true
         }
     }
-    sideDrawerClosedHandler = () => {
+    sideDrawerClosedHandler = () =>{ 
         this.setState({showSideDrawer: false});
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState ) => {
+            return { showSideDrawer: !prevState.showSideDrawer }
+        });   
     }
     render(){
         return (
             <Aux>
-            <TooBar />
+            <TooBar drawerToggleClicked={this.sideDrawerToggleHandler} />
             <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
                 <main className={styles.Content}> 
                     {this.props.children}
