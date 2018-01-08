11ce045a1d30d01fe945856e26d5691c6aedcbef
deepaliprasad <deepaliprasad123@gmail.com>
2018-01-08 12:14:55 +0530

    handled response error

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -8,13 +8,18 @@ const withErrorHandler = (WrappedComponent, axios) => {
             error: null
         }
         componentWillMount() {
-            axios.interceptors.request.use(req => {
+            this.reqIncerceptors = axios.interceptors.request.use(req => {
                 this.setState({error: null});
                 return req;
             })   
-            axios.interceptors.response.use( res => res, error => {
+            this.resInterceptors = axios.interceptors.response.use( res => res, error => {
                 this.setState({error: error});
             })
+        }
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqIncerceptors);
+            axios.interceptors.response.eject(this.resInterceptors);
         }
 
         errorConfirmedHandler = () => {
