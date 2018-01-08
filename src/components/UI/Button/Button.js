98b92235b36248c2974d44eb0b6124b8a5d4ae31
deepaliprasad <deepaliprasad123@gmail.com>
2018-01-08 12:13:30 +0530

    indentation change

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,7 +3,8 @@ import classes from './Button.css';
 
 
 const button = (props) => (
-    <button className={[classes.Button, classes[props.buttonType]].join(' ')} onClick={props.clicked}> {props.children} </button> 
+    <button className={[classes.Button, classes[props.buttonType]].join(' ')} 
+    onClick={props.clicked}> {props.children} </button> 
 );
 
 export default button;
