b23bab79d852213051a4aaad73100700a2e91731
deepalikumar <deepaliprasad123@gmail.com>
2018-01-10 16:31:23 +0530

    added seperate component for the order

---
+++ src/components/Order/Order.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './Order.css';
+
+
+const order = (props) => (
+    <div className={classes.Order}>  
+        <p> Ingredients : bacon(1)</p>
+    </div>
+);
+
+
+export default order;
+
