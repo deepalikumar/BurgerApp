a7b16cea5b589a1bf3ab0fc7747ce75e4a92c410
deepalikumar <deepaliprasad123@gmail.com>
2018-02-15 14:46:12 +0530

    added actions for fetching orderv

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -31,7 +31,7 @@ export const purchaseBurger = (orderData) => {
             console.log(response.data);
             dispatch(purchaseBurgerSuccess(response.data.name, orderData))
          }).catch(error => {
-             this.setState({loading: false})
+             dispatch(purchaseBurgerFailed(error))
      });
     }
 }
@@ -40,5 +40,46 @@ export const purchaseBurger = (orderData) => {
 export const purchaseInit = () => {
     return {
         type: actionTypes.PURCHASE_INIT
+    }
+}
+
+
+export const fetchOrdersSuccess = (orders) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_SUCCESS,
+        orders: orders
+    }
+}
+
+
+export const fetchOrdersFailed = (error) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_FAILED,
+        error: error
+    }
+}
+
+export const fetchOrdersStart = () => {
+    return {
+        type: actionTypes.FETCH_ORDERS_START
+    }
+}
+
+
+export const fetchOrders = () => {
+    return dispatch => {
+        dispatch(fetchOrdersStart());
+        axios.get('./orders.json')
+        .then(response => {
+            const fetchedOrders  = [];
+            for (let key in response.data) {
+                fetchedOrders.push({...response.data[key], id: key});
+            }
+            dispatch(fetchOrdersSuccess(fetchedOrders))
+        })
+        .catch(error => {
+            dispatch(fetchOrdersFailed(error));
+        })
+
     }
 }
