e006e507e589f1e6409d59561693620f7d26523b
deepalikumar <deepaliprasad123@gmail.com>
2018-02-21 14:42:57 +0530

    guarding the routes for authenticated and unauthenticated users also setting default page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layouts/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import { Route, Switch, withRouter} from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect} from 'react-router-dom';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
@@ -14,20 +14,40 @@ class App extends Component {
   componentDidMount() {
     this.props.onTryAutoSignUp();
   }
+  
   render() {
+    let routes = (
+        <Switch>
+          <Route path="/auth" component={Auth} />
+          <Route path="/" component={BurgerBuilder} />
+          <Redirect to="/" />
+        </Switch>
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
+      <Switch>
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/orders" component={Orders} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/" component={BurgerBuilder} />
+        <Redirect to="/" />
+      </Switch>
+      )
+    };
     return (
       <div>
         <Layout>
-            <Switch>
-              <Route path="/checkout" component={Checkout} />
-              <Route path="/orders" component={Orders} />
-              <Route path="/auth" component={Auth} />
-              <Route path="/logout" component={Logout} />
-              <Route path="/" component={BurgerBuilder} />
-            </Switch>
+          {routes}
         </Layout>
       </div>
     );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
   }
 }
 
@@ -36,4 +56,4 @@ const mapDispatchToProps = (dispatch) => {
     onTryAutoSignUp: () => dispatch(actions.authCheckState())
   }
 }
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
