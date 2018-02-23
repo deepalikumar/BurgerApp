a960cc0e09e45a61b95cf27192b3c5336b9ce4dc
deepalikumar <deepaliprasad123@gmail.com>
2018-02-23 11:38:36 +0530

    removing console.log

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -9,7 +9,6 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 class Orders extends Component {
    
     componentDidMount() {
-        console.log("inside component did mount", this.props.token);
         this.props.onFetchOrders(this.props.token, this.props.userId);
          }
     render () {
