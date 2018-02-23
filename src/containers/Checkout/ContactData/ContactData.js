b4ad6b612531f5850174c7a94a4d0adc5aa46fdb
deepalikumar <deepaliprasad123@gmail.com>
2018-02-23 11:37:53 +0530

    refactoring: commenting import which has not being used

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,7 +7,7 @@ import Input from '../../../components/UI/Input/Input';
 import { connect } from 'react-redux';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
-import { updateObject } from '../../../shared/utility';
+import { updateObject, checkValidity } from '../../../shared/utility';
 
 class ContactData extends Component {
     state = {
@@ -116,40 +116,13 @@ class ContactData extends Component {
          this.props.onOrderBurger(order, this.props.token);
     }
 
-    checkValidity(value, rules) {
-        let isValid = true;
-
-        if(!rules) {
-            return true;
-        }
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if(rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid;
-        }
-
-        if(rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid;
-        }
-        // if(rules.isEmail) {
-        //     const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+
-        //     isValid = 
-        // }
-
-        if(rules.isNumeric) {
-            const pattern = /^\d+$/;
-            isValid = pattern.test(value) && isValid;
-        }
-        return isValid;
-    }
+    
     inputChangedHandler = (event, controlName ) => {
         
         const updatedFormElement = updateObject(this.state.orderForm[controlName], 
             {
                 value:  event.target.value,
-                valid:  this.checkValidity(event.target.value, this.state.orderForm[controlName].validation),
+                valid:  checkValidity(event.target.value, this.state.orderForm[controlName].validation),
                 touched: true 
             });
         const updatedOrderForm = updateObject(this.state.orderForm, {
